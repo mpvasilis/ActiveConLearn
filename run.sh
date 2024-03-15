99f8de947befe358e1de60674a7878368bbf4191
Vasilis Balafas <me@vasilis.pw>
2024-03-15 15:30:38 +0200

    Added a benchmark and a run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#vgc
+-a mquacq2-a -b vgc -qg pqgen -exp b01_29_13_26_11_GreaterThanSudoku_9x9_24b_diverse -i C:\Users\Balafas\IdeaProjects\VM_Placement\modules\benchmarks\b01_29_13_26_11_GreaterThanSudoku_9x9_24b_diverse/ --output C:\Users\Balafas\IdeaProjects\VM_Placement/src/outputs/GtS_2 --useCon True
+
+#custom
+-a mquacq2-a -b custom -qg pqgen -exp b01_29_13_26_11_GreaterThanSudoku_9x9_24b_diverse -i C:\Users\Balafas\IdeaProjects\VM_Placement\modules\benchmarks\b01_29_13_26_11_GreaterThanSudoku_9x9_24b_diverse/ --output C:\Users\Balafas\IdeaProjects\VM_Placement/src/outputs/GtS_2 --useCon True
+
+#onlyActive
+-a mquacq2-a -b custom -qg pqgen -exp b01_29_13_26_11_GreaterThanSudoku_9x9_24b_diverse -i C:\Users\Balafas\IdeaProjects\VM_Placement\modules\benchmarks\b01_29_13_26_11_GreaterThanSudoku_9x9_24b_diverse/ --output C:\Users\Balafas\IdeaProjects\VM_Placement/src/outputs/GtS_2 --useCon True
