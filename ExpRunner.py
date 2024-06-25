14333fc3bbbdaa3c552b28439e0e7d5f4fc07ec0
Vasilis Balafas <me@vasilis.pw>
2024-06-25 14:25:56 +0300

    Version 0.2 Integrated GenAcq and MINE&ASK

--- ExpRunner.py
+++ ExpRunner.py
@@ -114,8 +114,8 @@ if __name__ == "__main__":
         {"algo": "mquacq2-a", "bench": "vgc", "onlyActive": False, "emptyCL": True, "type": "pl_al_genacq"},# pl + al + genacq
          {"algo": "mquacq2-a", "bench": "custom", "onlyActive": False, "emptyCL": False, "type": "pl_al"},#pl + al
          {"algo": "mquacq2-a", "bench": "custom", "onlyActive": True, "emptyCL": False, "type": "al"},# al
-        {"algo": "mquacq2-a", "genacq": "custom", "onlyActive": True, "emptyCL": False, "type": "genacq"}, #genacq
-        {"algo": "mquacq2-a", "mineask": "custom", "onlyActive": True, "emptyCL": False, "type": "mineask"} #mineask
+        {"algo": "mquacq2-a", "bench": "genacq", "onlyActive": True, "emptyCL": False, "type": "genacq"}, #genacq
+        {"algo": "mquacq2-a", "bench": "mineask", "onlyActive": True, "emptyCL": False, "type": "mineask"} #mineask
     ]
 
     if args.parallel:
